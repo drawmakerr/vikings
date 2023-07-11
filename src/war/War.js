2b2648e42be729d7e01fa487a73a81f8d6123fdd
Miguel <miguelpechero80@gmail.com>
2023-07-11 12:31:55 +0200

    viking war

--- src/war/War.js
+++ src/war/War.js
@@ -1,3 +1,23 @@
-class War {}
+
+class War {
+    constructor() {
+        this.vikingArmy = []
+        this.saxonArmy = []
+}
+addViking(viking){
+    this.vikingArmy.push(viking)
+}
+addSaxon(saxon){
+    this.saxonArmy.push(saxon)
+}
+vikingAttack(){
+    
+}
+
+
+}
+
+
+
 
 module.exports = War;
